b230e11bda00e4b285bfa94fbe5c72cf8789f457
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2026-01-06 08:48:03 +0100

    refactor(backend): introduce a wordslugger service

---
+++ backend/src/Service/Data/WordSlugger.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Service\Data;
+
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+/**
+ * @author Wilhelm Zwertvaegher
+ */
+class WordSlugger implements WordSluggerInterface
+{
+    public function __construct(private readonly SluggerInterface $slugger)
+    {
+    }
+
+    public function slug(string $str): string
+    {
+        return strtolower($this->slugger->slug(trim($str)));
+    }
+
+}
