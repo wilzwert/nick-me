98e3f0c3eefd21f523ec2753f8b8363ae3487e6a
Wilhelm Zwertvaegher <wilzwert@gmail.com>
2026-01-23 15:53:47 +0100

    fix(frontend): toasts count of 0 displayed instead of hiding ToastContainer

--- frontend/src/presentation/components/ToastContainer.tsx
+++ frontend/src/presentation/components/ToastContainer.tsx
@@ -5,7 +5,7 @@ export function ToastContainer() {
   const toasts = useToastStore((s) => s.toasts);
 
   return (
-    toasts.length && <div style={{ position: "fixed", bottom: 60, left: 0, right: 0, padding: "10px", textAlign: 'center', zIndex: 1000, backgroundColor: 'var(--mantine-color-body)' }}>
+    toasts.length > 0 && <div style={{ position: "fixed", bottom: 60, left: 0, right: 0, padding: "10px", textAlign: 'center', zIndex: 1000, backgroundColor: 'var(--mantine-color-body)' }}>
       {toasts.map((toast) => (
         <div
           key={toast.id}
