f43011d80fdf0e8cb45a60859123b9d71b4417bf
Wilhelm Zwertvaegher <wilzwert@gmail.com>
2026-02-06 11:24:06 +0100

    test(frontend): add some logging in playwright tests to debug staging workflow

--- frontend/tests/e2e/fixtures/env.ts
+++ frontend/tests/e2e/fixtures/env.ts
@@ -1,11 +1,24 @@
-import { test as base } from '@playwright/test';
+import { test as base, Page } from '@playwright/test';
 
 export type EnvFixtures = {
   isMockedApi: boolean;
 };
 
-export const test = base.extend<EnvFixtures>({
+export const test = base.extend<EnvFixtures, Page>({
   isMockedApi: async ({}, use) => {
     await use(process.env.MOCK_API === 'true');
+  },
+  page: async ({ page }, use) => {
+    page.on('console', msg => {
+      switch(msg.type()) {
+        case 'info':
+        case 'log':
+        case 'error':
+          console.log(`[page console.log] ${msg.type()} ${msg.text()}`); 
+      }
+      
+    });
+
+    await use(page);
   },
 });
