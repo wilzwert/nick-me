0fc32966de10237df8c487aac4db64c408ed2cfd
Wilhelm Zwertvaegher <144455434+wilzwert@users.noreply.github.com>
2026-03-01 20:32:20 +0100

    feat/design-update (#43)
    * feat(frontend): change fonts and styling
    
    * feat(frontend): change fonts and styling

--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from "@mantine/core";
 // theme.ts
 export const theme = createTheme({
   primaryColor: 'cyan',
-  fontFamily: 'Inter, sans-serif',
+  fontFamily: "'Major Mono Display', monospace",
   radius: {
     sm: '4px',
     md: '8px',
@@ -11,15 +11,31 @@ export const theme = createTheme({
   autoContrast: true,
   
   headings: {
-    fontFamily: 'Poppins, sans-serif',
+    // fontFamily: 'Poppins, sans-serif',
+    fontFamily: "'Press Start 2P', sans-serif",
     sizes: {
       h1: { fontSize: '66px', fontWeight: '600' },
     },
   },
+
   components: {
       Card: {
+        styles: () => ({
+          root: {
+            backgroundColor: 'light-dark(var(--mantine-color-white), var(--mantine-color-black))',
+            border: '4px solid light-dark(black, white)',
+            boxShadow: '6px 6px 0 black',
+            transition: 'transform 100ms ease',
+
+            '&:hover': {
+              transform: 'translate(-2px, -2px)',
+              boxShadow:
+                '8px 8px 0 light-dark(black, white)',
+            },
+          },
+        }),
         defaultProps: {
-          radius: 'md',
+          radius: 'none',
           shadow: 'sm',
           padding: 'lg',
         },
